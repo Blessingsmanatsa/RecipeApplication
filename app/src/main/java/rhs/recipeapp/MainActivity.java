bd3a6c96604cb708a49815d2d6c40dafeec443a3
Rachel <rdejaen@gmail.com>
2017-05-19 08:52:28 -0700

    trying to test sqlite info

--- app/src/main/java/rhs/recipeapp/MainActivity.java
+++ app/src/main/java/rhs/recipeapp/MainActivity.java
@@ -33,11 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Create a new map of values, where column names are the keys
         ContentValues values = new ContentValues();
-        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_RECIPE_NAME.recipeName);
-        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_TIMES.times);
-        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_NUM_SERVINGS.numServings);
-        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_INGREDIENTS.ingredients);
-        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_INSTRUCTIONS.instructions);
+        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_RECIPE_NAME, "recipeName");
+        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_TIMES, "times");
+        values.put(DatabaseContract.MyRecipes.COlUMN_NAME_NUM_SERVINGS, "numServings");
+        values.put(DatabaseContract.MyRecipes.COlUMN_NAME_INGREDIENTS, "ingredients");
+        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_INSTRUCTIONS, "instructions");
         // Insert the new row, returning the primary key value of the new row
         long newRowId = db.insert(DatabaseContract.MyRecipes.TABLE_NAME, null, values);
     }

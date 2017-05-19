b75c04656c385c8e5269c2c5ed4ce625de8c2b1e
Rachel <rdejaen@gmail.com>
2017-05-19 09:33:47 -0700

    connected the add recipe info activity to the browse activity

--- app/src/main/java/rhs/recipeapp/RecipeInput.java
+++ app/src/main/java/rhs/recipeapp/RecipeInput.java
@@ -1,11 +1,13 @@
 package rhs.recipeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class RecipeInput extends AppCompatActivity {
 
@@ -16,6 +18,15 @@ public class RecipeInput extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        Button button = (Button) findViewById(R.id.button); //creates the button to continue to the next activity
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +35,12 @@ public class RecipeInput extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void save() { //save this to sqlite database
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
+
     }
 
 }

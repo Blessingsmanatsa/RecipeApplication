c5e263a4d52aeeccc3034521e3c64e93f307b3d4
Rachel <rdejaen@gmail.com>
2017-05-24 09:34:20 -0700

    still trying to save, not working yet

--- app/src/main/java/rhs/recipeapp/MainActivity.java
+++ app/src/main/java/rhs/recipeapp/MainActivity.java
@@ -25,21 +25,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Create new helper
-        DatabaseHelper dbHelper = new DatabaseHelper(getApplicationContext());
-        // Get the database. If it does not exist, this is where it will
-        // also be created.
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        // Create a new map of values, where column names are the keys
-        ContentValues values = new ContentValues();
-        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_RECIPE_NAME, "recipeName");
-        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_TIMES, "times");
-        values.put(DatabaseContract.MyRecipes.COlUMN_NAME_NUM_SERVINGS, "numServings");
-        values.put(DatabaseContract.MyRecipes.COlUMN_NAME_INGREDIENTS, "ingredients");
-        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_INSTRUCTIONS, "instructions");
-        // Insert the new row, returning the primary key value of the new row
-        long newRowId = db.insert(DatabaseContract.MyRecipes.TABLE_NAME, null, values);
     }
 
     private void goToSecondActivity() {
--- app/src/main/java/rhs/recipeapp/RecipeInput.java
+++ app/src/main/java/rhs/recipeapp/RecipeInput.java
@@ -1,6 +1,9 @@
 package rhs.recipeapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,6 +14,8 @@ import android.widget.Button;
 
 public class RecipeInput extends AppCompatActivity {
 
+    private SQLiteOpenHelper mDbHelper = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +24,13 @@ public class RecipeInput extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-//        Button button = (Button) findViewById(R.id.button); //creates the button to continue to the next activity
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                save();
-//            }
-//        });
+        Button saveB = (Button) findViewById(R.id.button); //creates the button to save
+        saveB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveToDatabase();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -37,10 +42,22 @@ public class RecipeInput extends AppCompatActivity {
         });
     }
 
-//    private void save() { //save this to sqlite database
-//        Intent intent = new Intent(this, WelcomeActivity.class);
-//        startActivity(intent);
-//
-//    }
+    private void saveToDatabase() { //save this to sqlite database
+        // Create new helper
+        DatabaseHelper dbHelper = new DatabaseHelper(getApplicationContext());
+        // Get the database. If it does not exist, this is where it will
+        // also be created.
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_RECIPE_NAME, "recipeName");
+        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_TIMES, "times");
+        values.put(DatabaseContract.MyRecipes.COlUMN_NAME_NUM_SERVINGS, "numServings");
+        values.put(DatabaseContract.MyRecipes.COlUMN_NAME_INGREDIENTS, "ingredients");
+        values.put(DatabaseContract.MyRecipes.COLUMN_NAME_INSTRUCTIONS, "instructions");
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(DatabaseContract.MyRecipes.TABLE_NAME, null, values);
+    }
 
 }

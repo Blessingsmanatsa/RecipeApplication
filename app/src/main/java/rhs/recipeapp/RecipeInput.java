43852dd2326b0f48669faa70e5bbd6937e8a156b
Rachel <rdejaen@gmail.com>
2017-06-12 09:17:47 -0700

    fixed the errors from the colors

--- app/src/main/java/rhs/recipeapp/RecipeInput.java
+++ app/src/main/java/rhs/recipeapp/RecipeInput.java
@@ -25,8 +25,6 @@ public class RecipeInput extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
